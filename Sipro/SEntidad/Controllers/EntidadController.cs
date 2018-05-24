cfedd4e8507e1a6eae3c3a0d208c9ed1fc18f90f
ppenan <pedro_penan@hotmail.com>
2018-05-24 15:48:50 -0600

    Se realizan cambios en controlador de entidad

--- Sipro/SEntidad/Controllers/EntidadController.cs
+++ Sipro/SEntidad/Controllers/EntidadController.cs
@@ -1,17 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SiproModelCore.Models;
+using SiproDAO.Dao;
+using Utilities;
+using Microsoft.AspNetCore.Cors;
 
 namespace SEntidad.Controllers
 {
     [Authorize]
     [Produces("application/json")]
     [Route("/api/[controller]/[action]")]
+    [EnableCors("AllowAllHeaders")]
     public class EntidadController : Controller
     {
-        
+        // POST api/Entidad/Entidades
+        [HttpPost]
+        [Authorize("Entidades - Visualizar")]
+        public IActionResult Entidades([FromBody]dynamic value)
+        {
+            try
+            {
+                List<Entidad> lstentidades = EntidadDAO.getEntidadesPagina((int)value.pagina, (int)value.registros, (string)value.filtro_entidad, (string)value.filtro_nombre,
+                    (string)value.filtro_abreviatura, (string)value.columna_ordenada, (string)value.orden_direccion);
+
+                return Ok(new { success = lstentidades != null ? true : false, entidades = lstentidades });
+            }
+            catch (Exception e)
+            {
+                CLogger.write("1", "EntidadController.class", e);
+                return BadRequest(500);
+            }
+        }
+
+        // GET api/Entidad/EntidadesPorEjercicio/ejercicio
+        [HttpGet("{ejercicio}")]
+        [Authorize("Entidades - Visualizar")]
+        public IActionResult EntidadesPorEjercicio(int ejercicio)
+        {
+            try
+            {
+                List<Entidad> lstentidades = EntidadDAO.getEntidades(ejercicio);
+
+                return Ok(new { success = lstentidades != null ? true : false, entidades = lstentidades });
+            }
+            catch (Exception e)
+            {
+                CLogger.write("2", "EntidadController.class", e);
+                return BadRequest(500);
+            }
+        }
+
+        // POST api/Entidad/Entidad
+        [HttpPost]
+        [Authorize("Entidades - Crear")]
+        public IActionResult Entidad([FromBody]dynamic value)
+        {
+            try
+            {
+                bool creado = EntidadDAO.guardarEntidad((int)value.entidad, (int)value.ejercicio, (string)value.nombre, (string)value.abreviatura);
+
+                if (creado)
+                {
+                    List<Entidad> lstentidades = EntidadDAO.getEntidadesPagina((int)value.pagina, (int)value.registros, (string)value.filtro_entidad, (string)value.filtro_nombre,
+                    (string)value.filtro_abreviatura, (string)value.columna_ordenada, (string)value.orden_direccion);
+
+                    return Ok(new { success = true, entidades = lstentidades });
+                }
+                else
+                    return Ok(new { success = false });
+            }
+            catch (Exception e)
+            {
+                CLogger.write("3", "EntidadController.class", e);
+                return BadRequest(500);
+            }
+        }
+
+        // POST api/Entidad/Entidad
+        [HttpPost]
+        [Authorize("Entidades - Editar")]
+        public IActionResult EntidadA([FromBody]dynamic value)
+        {
+            try
+            {
+                bool actualizado = EntidadDAO.guardarEntidad((int)value.entidad, (int)value.ejercicio, null,(string)value.abreviatura);
+
+                if (actualizado)
+                {
+                    List<Entidad> lstentidades = EntidadDAO.getEntidadesPagina((int)value.pagina, (int)value.registros, (string)value.filtro_entidad, (string)value.filtro_nombre,
+                    (string)value.filtro_abreviatura, (string)value.columna_ordenada, (string)value.orden_direccion);
+
+                    return Ok(new { success = true, entidades = lstentidades });
+                }
+                else
+                    return Ok(new { success = false });
+            }
+            catch (Exception e)
+            {
+                CLogger.write("4", "EntidadController.class", e);
+                return BadRequest(500);
+            }
+        }
+
+        // POST api/Entidad/totalEntidades
+        [HttpPost]
+        [Authorize("Entidades - Visualizar")]
+        public IActionResult totalEntidades([FromBody]dynamic value)
+        {
+            try
+            {
+                long total = EntidadDAO.getTotalEntidades((string)value.filtro_entidad, (string)value.filtro_nombre, (string)value.filtro_abreviatura);
+
+                return Ok(new { success = true, total = total });
+            }
+            catch (Exception e)
+            {
+                CLogger.write("5", "EntidadController.class", e);
+                return BadRequest(500);
+            }
+        }
     }
 }
--- Sipro/SEntidad/Startup.cs
+++ Sipro/SEntidad/Startup.cs
@@ -1,13 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Utilities;
+using SiproModelCore.Models;
+using Identity;
+using Microsoft.AspNetCore.Identity;
+using Dapper;
+using Microsoft.AspNetCore.DataProtection;
+using System.IO;
+using System.Net;
 
 namespace SEntidad
 {
@@ -16,6 +20,10 @@ namespace SEntidad
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            var mapper = (SqlMapper.ITypeMap)Activator
+                .CreateInstance(typeof(ColumnAttributeTypeMapper<>)
+                .MakeGenericType(typeof(Entidad)));
+            SqlMapper.SetTypeMap(typeof(Entidad), mapper);
         }
 
         public IConfiguration Configuration { get; }
@@ -24,6 +32,71 @@ namespace SEntidad
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddIdentity<User, Rol>()
+                .AddRoleStore<RoleStore>()
+                .AddUserStore<UserPasswordStore>()
+                .AddUserManager<CustomUserManager>()
+                .AddDefaultTokenProviders();
+
+            services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo(@"/SIPRO"))
+                    .SetApplicationName("SiproApp")
+                    .DisableAutomaticKeyGeneration();
+
+            services.ConfigureApplicationCookie(options => {
+                options.Cookie.Name = ".AspNet.Sipro";
+                options.Cookie.HttpOnly = true;
+
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    if (context.Response.StatusCode == (int)HttpStatusCode.OK)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    }
+                    else
+                    {
+                        context.Response.Redirect(context.RedirectUri);
+                    }
+                    return Task.CompletedTask;
+                };
+
+                options.Events.OnRedirectToAccessDenied = context =>
+                {
+                    if (context.Response.StatusCode == (int)HttpStatusCode.OK)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    }
+                    else
+                    {
+                        context.Response.Redirect(context.RedirectUri);
+                    }
+                    return Task.CompletedTask;
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Entidades - Visualizar",
+                                  policy => policy.RequireClaim("sipro/permission", "Entidades - Visualizar"));
+                options.AddPolicy("Entidades - Editar",
+                                  policy => policy.RequireClaim("sipro/permission", "Entidades - Editar"));
+                options.AddPolicy("Entidades - Eliminar",
+                                  policy => policy.RequireClaim("sipro/permission", "Entidades - Eliminar"));
+                options.AddPolicy("Entidades - Crear",
+                                  policy => policy.RequireClaim("sipro/permission", "Entidades - Crear"));
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllHeaders",
+                      builder =>
+                      {
+                          builder.AllowAnyOrigin()
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
+                      });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
